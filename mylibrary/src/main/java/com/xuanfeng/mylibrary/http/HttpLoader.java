995e3ca6243e87ca683d0cd688bd5b9053539853
xuanfengwuxiang <630455664@qq.com>
2018-12-17 15:31:43 +0800

    retrofit统一添加头信息

--- mylibrary/src/main/java/com/xuanfeng/mylibrary/http/HttpLoader.java
+++ mylibrary/src/main/java/com/xuanfeng/mylibrary/http/HttpLoader.java
@@ -1,8 +1,12 @@
 package com.xuanfeng.mylibrary.http;
 
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
+import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
@@ -21,14 +25,9 @@ public class HttpLoader {
     private final HttpService mHttpService;
 
     private HttpLoader() {
-        HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
-        loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
-        OkHttpClient client = new OkHttpClient.Builder()
-                .connectTimeout(TIME_OUT, TimeUnit.SECONDS)
-                .addInterceptor(loggingInterceptor)
-                .build();
+
         mHttpService = new Retrofit.Builder()
-                .client(client)
+                .client(getOkHttpClient())
                 .addConverterFactory(GsonConverterFactory.create())//Gson转换
                 .addConverterFactory(ScalarsConverterFactory.create())//String转换
                 .addCallAdapterFactory(RxJava2CallAdapterFactory.create())//请求回参设为RxJava
@@ -36,6 +35,7 @@ public class HttpLoader {
                 .build()
                 .create(HttpService.class);
     }
+
 
     //单例、双重检验加锁
     public static synchronized HttpLoader getInstance() {
@@ -52,5 +52,31 @@ public class HttpLoader {
     //获取接口
     public HttpService getService() {
         return mHttpService;
+    }
+
+
+    //返回okHttp客户端
+    private OkHttpClient getOkHttpClient() {
+        return new OkHttpClient.Builder()
+                .connectTimeout(TIME_OUT, TimeUnit.SECONDS)
+                .addInterceptor(new HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))//log日志
+                .addInterceptor(getHeaders())//请求头
+                .build();
+    }
+
+    //添加统一请求头
+    private Interceptor getHeaders() {
+
+        return new Interceptor() {
+            @Override
+            public Response intercept(Chain chain) throws IOException {
+                Request request = chain.request()
+                        .newBuilder()
+                        .addHeader("test", "test")
+                        .build();
+                return chain.proceed(request);
+            }
+        };
+
     }
 }
