bd8b8f3cc7ad0a5ce26c886322c52c62a33a0362
xuanfengwuxiang <630455664@qq.com>
2019-04-13 15:30:28 +0800

    新增减肥代谢以及燃脂心率计算工具

--- app/src/main/java/com/xuanfeng/xuanfengweather/module/menu/MenuFragment.java
+++ app/src/main/java/com/xuanfeng/xuanfengweather/module/menu/MenuFragment.java
@@ -3,31 +3,17 @@ package com.xuanfeng.xuanfengweather.module.menu;
 import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.LinearLayout;
-import android.widget.TextView;
+import android.view.View;
 
 import com.xuanfeng.mylibrary.mvp.BaseFragment;
 import com.xuanfeng.xuanfengweather.R;
+import com.xuanfeng.xuanfengweather.module.loseweight.LoseWeightCalculatorActivity;
 import com.xuanfeng.xuanfengweather.module.test.activity.TestActivity;
 
-import butterknife.BindView;
 import butterknife.OnClick;
 
-
+//侧滑界面
 public class MenuFragment extends BaseFragment {
-
-    @BindView(R.id.adview_container)
-    LinearLayout mAdviewContainer;
-
-    @BindView(R.id.tv_test)
-    TextView tvTest;
-
-    //测试按钮
-    @OnClick(R.id.tv_test)
-    public void onViewClicked() {
-        Intent intent = new Intent(getContext(), TestActivity.class);
-        startActivity(intent, ActivityOptions.makeSceneTransitionAnimation(getActivity()).toBundle());
-    }
 
     @Override
     public int getLayoutId() {
@@ -42,5 +28,20 @@ public class MenuFragment extends BaseFragment {
     @Override
     public void initData(Bundle bundle) {
 
+    }
+
+
+    @OnClick({R.id.tv_lose_weight, R.id.tv_test})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.tv_lose_weight:
+                Intent intent = new Intent(getContext(), LoseWeightCalculatorActivity.class);
+                startActivity(intent, ActivityOptions.makeSceneTransitionAnimation(getActivity()).toBundle());
+                break;
+            case R.id.tv_test:
+                intent = new Intent(getContext(), TestActivity.class);
+                startActivity(intent, ActivityOptions.makeSceneTransitionAnimation(getActivity()).toBundle());
+                break;
+        }
     }
 }
