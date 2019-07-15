d2d02c6bc9244d04d0ee335d023f8fb855544709
19042935 <630455664@qq.com>
2019-07-15 12:37:38 +0800

    切换成databinding

--- app/src/main/java/com/xuanfeng/weather/module/media/fragment/ChatFragment.java
+++ app/src/main/java/com/xuanfeng/weather/module/media/fragment/ChatFragment.java
@@ -12,6 +12,7 @@ import com.xuanfeng.mylibrary.mvp.BaseActivity;
 import com.xuanfeng.mylibrary.mvp.BaseFragment;
 import com.xuanfeng.mylibrary.mvp.BasePresenter;
 import com.xuanfeng.weather.R;
+import com.xuanfeng.weather.databinding.FragmentChatBinding;
 import com.xuanfeng.weather.module.media.model.ChatModel;
 import com.xuanfeng.weather.module.media.presenter.ChatPresenter;
 import com.xuanfeng.weather.module.media.view.ChatView;
@@ -22,29 +23,22 @@ import butterknife.BindView;
 import butterknife.OnClick;
 
 //聊天界面
-public class ChatFragment extends BaseFragment<ChatPresenter, ViewDataBinding> implements ChatView {
+public class ChatFragment extends BaseFragment<ChatPresenter, FragmentChatBinding> implements ChatView {
 
 
-    @BindView(R.id.rv_chat)
-    ChatRecyclerView mRvChat;
 
-    @BindView(R.id.et_input)
-    EditText mEtInput;
-
-    @BindView(R.id.tv_send)
-    CustomTextView mTvSend;
     private ChatPresenter mChatPresenter;
 
 
     @OnClick(R.id.tv_send)
     public void onViewClicked() {
-        String content = mEtInput.getText().toString();
+        String content = mBinding.etInput.getText().toString();
         if (TextUtils.isEmpty(content)) {
             Toast.makeText(getContext(), "请输入聊天内容", Toast.LENGTH_SHORT).show();
             return;
         }
-        mRvChat.setData(new ChatBean("me", content));
-        mEtInput.getText().clear();
+        mBinding.rvChat.setData(new ChatBean("me", content));
+        mBinding.etInput.getText().clear();
         mChatPresenter.getReply(this, content);
     }
 
@@ -61,13 +55,13 @@ public class ChatFragment extends BaseFragment<ChatPresenter, ViewDataBinding> i
 
     @Override
     public void initData(Bundle bundle) {
-        mRvChat.setData(new ChatBean("you", "嗨，我是陪聊师##__##"));
+        mBinding.rvChat.setData(new ChatBean("you", "嗨，我是陪聊师##__##"));
     }
 
     @Override
     public void onGetReply(String content) {
         if (!TextUtils.isEmpty(content)) {
-            mRvChat.setData(new ChatBean("you", content));
+            mBinding.rvChat.setData(new ChatBean("you", content));
         }
     }
 
