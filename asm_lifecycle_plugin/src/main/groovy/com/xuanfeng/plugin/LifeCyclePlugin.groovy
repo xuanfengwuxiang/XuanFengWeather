5eb66d64100d6c8cc9ae00307c6c536920ee49d5
19042935 <630455664@qq.com>
2020-04-07 23:39:03 +0800

    自定义gradle实验

---
+++ asm_lifecycle_plugin/src/main/groovy/com/xuanfeng/plugin/LifeCyclePlugin.groovy
@@ -0,0 +1,14 @@
+package com.xuanfeng.plugin
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+
+public class LifeCyclePlugin implements Plugin<Project> {
+
+
+    @Override
+    void apply(Project project) {
+        System.out.println("LifeCyclePlugin gradle plugin===")
+
+    }
+}
