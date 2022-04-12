bf11e282617c90b8b57a2b743c2283b6b66ff330
朱建华 <630455664@qq.com>
2022-04-12 11:34:32 +0800

    定位工具提交

--- xflibrary/src/main/java/com/xuanfeng/xflibrary/utils/LocationUtil.java
+++ xflibrary/src/main/java/com/xuanfeng/xflibrary/utils/LocationUtil.java
@@ -1,8 +1,113 @@
 package com.xuanfeng.xflibrary.utils;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.provider.Settings;
+import android.widget.Toast;
+
+import androidx.core.app.ActivityCompat;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
 /**
  * 定位工具
  */
 public class LocationUtil {
+
+    private LocationUtil() {
+    }
+
+    /**
+     * 注册
+     * <p>使用完记得调用{@link #unregister(Context, LocationListener)} ()}</p>
+     * <p>需添加权限 {@code <uses-permission android:name="android.permission.INTERNET"/>}</p>
+     * <p>需添加权限 {@code <uses-permission android:name="android.permission.ACCESS_COARSE_LOCATION"/>}</p>
+     * <p>需添加权限 {@code <uses-permission android:name="android.permission.ACCESS_FINE_LOCATION"/>}</p>
+     * <p>如果{@code minDistance}为0，则通过{@code minTime}来定时更新；</p>
+     * <p>{@code minDistance}不为0，则以{@code minDistance}为准；</p>
+     * <p>两者都为0，则随时刷新。</p>
+     *
+     * @param activity            activity
+     * @param minTime             位置信息更新周期（单位：毫秒）
+     * @param minDistance         位置变化最小距离：当位置距离变化超过此值时，将更新位置信息（单位：米）
+     * @param listener            位置刷新的回调接口
+     * @param locationRequestCode 请求权限的requestCode
+     * @return {@code true}: 初始化成功<br>{@code false}: 初始化失败
+     */
+    public static void register(Activity activity, long minTime, long minDistance, LocationListener listener, int locationRequestCode) {
+        if (activity == null) {
+            return;
+        }
+
+        LocationManager locationManager = (LocationManager) activity.getSystemService(Context.LOCATION_SERVICE);
+        boolean providerEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        // GPS检查开启
+        if (!providerEnabled) {
+            Toast.makeText(activity, "请打开GPS", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // 检查权限
+        String[] permission = new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
+
+        if (ActivityCompat.checkSelfPermission(activity, permission[0]) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(activity, permission[1]) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(activity, permission, locationRequestCode);
+            return;
+        }
+
+
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, minTime, minDistance, listener);
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, minTime, minDistance, listener);
+        locationManager.requestLocationUpdates(LocationManager.PASSIVE_PROVIDER, minTime, minDistance, listener);
+
+    }
+
+    // 删除监听器
+    public static void unregister(Context context, LocationListener listener) {
+        LocationManager locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+        locationManager.removeUpdates(listener);
+    }
+
+    /**
+     * 根据经纬度获取地理位置
+     *
+     * @param context   上下文
+     * @param latitude  纬度
+     * @param longitude 经度
+     * @return {@link Address}
+     */
+    public static Address getAddress(Context context, double latitude, double longitude) {
+        Geocoder geocoder = new Geocoder(context, Locale.getDefault());
+        try {
+            List<Address> addresses = geocoder.getFromLocation(latitude, longitude, 1);
+            if (addresses.size() > 0)
+                return addresses.get(0);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * 打开Gps设置界面
+     *
+     * @param context context
+     */
+    public static void openGpsSettings(Context context) {
+        Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(intent);
+    }
+
 
 }
